77d61f51ec10adfd779cc97e635ad3bec065576b
antoinePPRD <antoine.poupard@etu-upsaclay.fr>
2025-01-02 21:27:36 +0000

    debug

--- main.h
+++ main.h
@@ -4,6 +4,6 @@ struct stlifestyle
     int id[10000];
     int physical_activity[10000];
     float alcohol_consumption[10000];
-    float affeine_consumption[10000];
+    float caffeine_consumption[10000];
     float sleep_quality[10000];
 };
