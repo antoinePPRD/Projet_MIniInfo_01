62f1d62bdbb58603ef878df4a15daab2a19beeb0
antoinePPRD <antoine.poupard@etu-upsaclay.fr>
2025-01-02 15:52:55 +0000

    debug

--- Main.c
+++ Main.c
@@ -27,10 +27,11 @@ int main (){
 
     // calcul le nombre de ligne du fichier 
     int car, NbLigne ;
+    car = 0 ;
     NbLigne = 1;
     while (( car = fgetc(lifestyle)) != EOF)
         if (car == '\n')
-        printf("%d", car);
+        printf("%c", car);
         ++NbLigne;
     printf("%d\n", NbLigne);
     fclose(lifestyle);
