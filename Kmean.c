3d1c849880ae03a8ca4e3c11c439788b37da1bf3
antoinePPRD <antoine.poupard@etu-upsaclay.fr>
2025-01-04 17:34:30 +0000

    test

--- Kmean.c
+++ Kmean.c
@@ -82,13 +82,8 @@ void analyserClusters(int *cluster1_ids, int nb_cluster1, int *cluster2_ids, int
         }
     }
 
-    float similariter_cluster = 0;
-    if (nb_cluster2 > nb_cluster1) {
-        similariter_cluster = (intersection/nb_cluster1)*100;
-    }
-    else {
-        similariter_cluster = (intersection/nb_cluster2)*100;
-    }
+    float similariter_cluster = (intersection/((nb_cluster1+nb_cluster2)/2))*100;
+
     printf("Meme IDs entre (%s) et (%s) : %f\n Pourcentage de similariter :%f\n", attribut1, attribut2, intersection, similariter_cluster);
     printf("-------------------------------------------------------\n");
 }
